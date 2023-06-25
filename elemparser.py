0e067f2cbc06e07417f4728f2eb4cb663ad5e47c
Vladimir <lisoff.vol@yandex.ru>
2023-06-25 11:20:45 +0300

    try

--- elemparser.py
+++ elemparser.py
@@ -10,20 +10,21 @@ def elem_parse(driver, elemSelector, companies):
         if(i < 50):
             link = element.find_element(By.CSS_SELECTOR, ".directory_profile")
             link.click()
-            driver.switch_to.window(driver.window_handles[1])
-            name = driver.find_element(By.CSS_SELECTOR, ".profile-header__title .website-link__item").text
-            site = driver.find_element(By.CSS_SELECTOR, ".website-link__item").get_attribute("href").split('?')[0]
-            facebook = ''
-            linkedIn = ''
-            if(len(driver.find_elements(By.CSS_SELECTOR, ".sg-social-media--link-linkedin")) > 0):
-                linkedIn = driver.find_element(By.CSS_SELECTOR, ".sg-social-media--link-linkedin").get_attribute("href")
-                if(len(driver.find_elements(By.CSS_SELECTOR, ".sg-social-media--link-facebook")) > 0):
-                    facebook = driver.find_element(By.CSS_SELECTOR, ".sg-social-media--link-facebook").get_attribute("href")
-                
-            else:
-                if(len(driver.find_elements(By.CSS_SELECTOR, ".sg-social-media--link-facebook")) > 0):
-                    facebook = driver.find_element(By.CSS_SELECTOR, ".sg-social-media--link-facebook").get_attribute("href")
-                
-            companies.append({'name': name, 'site': site, 'email': '', 'facebook': facebook, 'linkedin': linkedIn})
-            driver.close()
-            driver.switch_to.window(driver.window_handles[0])
+            try:
+                driver.switch_to.window(driver.window_handles[1])
+                name = driver.find_element(By.CSS_SELECTOR, ".profile-header__title .website-link__item").text
+                site = driver.find_element(By.CSS_SELECTOR, ".website-link__item").get_attribute("href").split('?')[0]
+                facebook = ''
+                linkedIn = ''
+                if(len(driver.find_elements(By.CSS_SELECTOR, ".sg-social-media--link-linkedin")) > 0):
+                    linkedIn = driver.find_element(By.CSS_SELECTOR, ".sg-social-media--link-linkedin").get_attribute("href")
+                    if(len(driver.find_elements(By.CSS_SELECTOR, ".sg-social-media--link-facebook")) > 0):
+                        facebook = driver.find_element(By.CSS_SELECTOR, ".sg-social-media--link-facebook").get_attribute("href")
+                    
+                else:
+                    if(len(driver.find_elements(By.CSS_SELECTOR, ".sg-social-media--link-facebook")) > 0):
+                        facebook = driver.find_element(By.CSS_SELECTOR, ".sg-social-media--link-facebook").get_attribute("href")
+                companies.append({'name': name, 'site': site, 'email': '', 'facebook': facebook, 'linkedin': linkedIn})
+            finally:
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
